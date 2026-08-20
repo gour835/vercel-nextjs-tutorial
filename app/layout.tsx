90fe05f5d90f3bce4957a1ebd59fd37658673ee7
Gouransh <gouranshnayak1234@gmail.com>
2026-08-20 14:11:49 +0530

    next js course

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,16 @@ import '@/app/ui/global.css';
 // import {inter} from '@/app/ui/fonts';
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
